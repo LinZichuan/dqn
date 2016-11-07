5493bb9101362a47dc0500ad29899cabbb9484ee
LinZichuan <linzichuan12@163.com>
2016-11-07 16:17:55 +0800

    Add GPU-use fraction.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from gymenv import GymEnv
 from agent import Agent
 
 def main():
-    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.33)
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
         config = Config()
         
